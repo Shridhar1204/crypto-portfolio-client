4c2ba2e4820b6d5e0e64752d807dc7fdd2defe6d
Shridhar1204 <sharmashridhar1204@gmail.com>
2025-08-07 21:44:09 +0530

    somehitn

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import  { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/ReactToastify.css';
 
-
+//main
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
